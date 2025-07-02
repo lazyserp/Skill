191e88e5efb431c6046c956d90ecdd374b9222f0
Aman <amanjha434@gmail.com>
2025-07-02 17:10:57 +0530

    Fixed BUgs

---
+++ backend/app/oauth.py
@@ -0,0 +1,15 @@
+from authlib.integrations.flask_client import OAuth
+
+oauth = OAuth()
+
+def configure_oauth(app):
+    oauth.init_app(app)
+    oauth.register(
+        name='google',
+        client_id=app.config['GOOGLE_CLIENT_ID'],
+        client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+        access_token_url='https://oauth2.googleapis.com/token',
+        authorize_url='https://accounts.google.com/o/oauth2/auth',
+        api_base_url='https://www.googleapis.com/oauth2/v1/',
+        client_kwargs={'scope': 'openid email profile'}
+    )
